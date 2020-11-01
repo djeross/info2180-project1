d03a442a20f0e2cb59be2fe8cd549698023a7ad2
Rodje Ross <rodjeross10@yahoo.com>
2020-11-01 18:08:46 -0500

    Final update (images and all files completed)

--- scripts.js
+++ scripts.js
@@ -1,1 +1,17 @@
 /* Add your JavaScript to this file */
+window.addEventListener('load',function(){
+    document.getElementsByClassName("btn")[1].addEventListener("click",doit);
+
+
+    function doit(event){
+        event.preventDefault();
+        var emailaddr=document.getElementById("email").value;
+        if(emailaddr.length==0){
+            document.getElementsByClassName("message")[0].innerHTML="Please enter a valid email address.";
+
+        }
+        else{
+            document.getElementsByClassName("message")[0].innerHTML="Thank you! Your email address "+emailaddr+" has been added to our mailing list.";
+        }
+    }
+})
